451a4b4153716f5aec936e20aa21cd380bb94288
Dusan Jovanov <d.jovanov@greco.services>
2020-12-05 16:41:24 +0100

    start

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Head>
+        <title>Hello</title>
+        <link rel="shortcut icon" href="https://www.favicon.cc/logo3d/435271.png" type="image/x-icon"/>
+      </Head>
+    </>
+  );
+}
